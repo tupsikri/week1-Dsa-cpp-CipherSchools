e4380314cc85c862ee8f32aa957314b0166cd64f
tupsikri <125789345+tupsikri@users.noreply.github.com>
2023-07-28 18:59:12 +0530

    Add files via upload

---
+++ lecture 5 array-- l.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	cout<<"Give 5 integers as input"<<endl;
+	cin>>a>>b>>c>>d>>e;
+	
+	cout<<"The integers in reverse order are:"<<e<<d<<c<<b<<a;
+	return 0;
+}
