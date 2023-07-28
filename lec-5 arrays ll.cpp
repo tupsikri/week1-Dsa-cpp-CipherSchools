97ce403c1b9830b6342a5bb734374ea1d89487b2
tupsikri <125789345+tupsikri@users.noreply.github.com>
2023-07-28 19:06:30 +0530

    Add files via upload

---
+++ lec-5 arrays ll.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[4];
+	cout<<"give five integers as a input"<<endl;
+	for(i=0;i<5;i++);
+	{
+		cin>>a[i];
+		
+	}
+	cout <<" the five given inputs are : ""<<endl;
+		for(i=0;i<5;i++);
+		{
+			cout<<a[i];
+		}
+	
+	return 0;
+}
